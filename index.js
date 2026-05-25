23a8927d0c95034abc4066722db68dbab08dadba
sujith kumar <thesujithkumar@gmail.com>
2026-05-25 21:56:37 +0530

    Updated application response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Pipeline Running 🚀");
+  res.send("CI/CD Pipeline Success🚀");
 });
 
 app.listen(3000, () => {
